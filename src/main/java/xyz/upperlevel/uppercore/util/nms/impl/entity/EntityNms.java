e5931719e2bf925bb71150dd0e8a3e86d8318951
Rossi Lorenzo <snowycoder@gmail.com>
2017-08-04 20:51:30 +0200

    Fix EntityNms bad indentation

--- src/main/java/xyz/upperlevel/uppercore/util/nms/impl/entity/EntityNms.java
+++ src/main/java/xyz/upperlevel/uppercore/util/nms/impl/entity/EntityNms.java
@@ -5,6 +5,7 @@ import xyz.upperlevel.uppercore.util.nms.NmsPacket;
 import xyz.upperlevel.uppercore.util.nms.exceptions.UnsupportedVersionException;
 import xyz.upperlevel.uppercore.util.nms.impl.TagNms;
 
+import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.function.Consumer;
 
@@ -14,35 +15,35 @@ public final class EntityNms {
     public static final Class<?> NMS_CLASS;
     public static final Class<?> BUKKIT_CLASS;
     private static final Method getHandle;
-    private static final Method entityGetTag;
-    private static final Method entitySetTag;
+    private static final Method getTag;
+    private static final Method setTag;
 
-   static {
-       try {
-           NMS_CLASS = NmsPacket.NMS.getClass("Entity");
-           BUKKIT_CLASS = NmsPacket.CRAFT_ENTITY.getClass("CraftEntity");
+    static {
+        try {
+            NMS_CLASS = NmsPacket.NMS.getClass("Entity");
+            BUKKIT_CLASS = NmsPacket.CRAFT_ENTITY.getClass("CraftEntity");
 
 
-           getHandle = BUKKIT_CLASS.getDeclaredMethod("getHandle");
-           entityGetTag = NMS_CLASS.getDeclaredMethod("c", TagNms.CLASS);
-           entitySetTag = NMS_CLASS.getDeclaredMethod("f", TagNms.CLASS);
-       } catch (Exception e) {
-           throw new UnsupportedVersionException(e);
-       }
-   }
+            getHandle = BUKKIT_CLASS.getDeclaredMethod("getHandle");
+            getTag = NMS_CLASS.getDeclaredMethod("c", TagNms.CLASS);
+            setTag = NMS_CLASS.getDeclaredMethod("f", TagNms.CLASS);
+        } catch (Exception e) {
+            throw new UnsupportedVersionException(e);
+        }
+    }
 
-   public static Object getHandle(Entity entity) {
-       try {
-           return getHandle.invoke(entity);
-       } catch (Exception e) {
-           handleException(e);
-           return null;
-       }
-   }
+    public static Object getHandle(Entity entity) {
+        try {
+            return getHandle.invoke(entity);
+        } catch (Exception e) {
+            handleException(e);
+            return null;
+        }
+    }
 
     public static Object getTag(Entity entity, Object tag) {
         try {
-            return entityGetTag.invoke(entity, tag);
+            return getTag.invoke(entity, tag);
         } catch (Exception e) {
             handleException(e);
             return null;
@@ -51,7 +52,7 @@ public final class EntityNms {
 
     public static void setTag(Entity entity, Object tag) {
         try {
-            entityGetTag.invoke(entity, tag);
+            getTag.invoke(entity, tag);
         } catch (Exception e) {
             handleException(e);
         }
@@ -61,9 +62,9 @@ public final class EntityNms {
         try {
             Object handle = EntityNms.getHandle(entity);
             Object compound = TagNms.create();
-            entityGetTag.invoke(handle, compound);
+            getTag.invoke(handle, compound);
             tagConsumer.accept(compound);
-            entitySetTag.invoke(handle, compound);
+            setTag.invoke(handle, compound);
         } catch (Exception e) {
             handleException(e);
         }
@@ -78,16 +79,17 @@ public final class EntityNms {
     }
 
     public static void setInvulnerable(Entity entity, boolean value) {
-        editTag(entity, tag -> TagNms.set(tag,"Invulnerable", value));
+        editTag(entity, tag -> TagNms.set(tag, "Invulnerable", value));
     }
 
     public static void freeze(Entity entity, boolean value) {
         editTag(entity, tag -> {
-            TagNms.set(tag,"NoAI", value);
+            TagNms.set(tag, "NoAI", value);
             TagNms.set(tag, "Silent", value);
             TagNms.set(tag, "Invulnerable", value);
         });
     }
 
-   private EntityNms(){}
+    private EntityNms() {
+    }
 }
