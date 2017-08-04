bed430799132a2b37ab8a387c80f427938dfa72d
Rossi Lorenzo <snowycoder@gmail.com>
2017-08-04 11:31:11 +0200

    Fix instant fireworks

--- src/main/java/xyz/upperlevel/uppercore/util/nms/impl/entity/FireworkNms.java
+++ src/main/java/xyz/upperlevel/uppercore/util/nms/impl/entity/FireworkNms.java
@@ -1,31 +1,21 @@
 package xyz.upperlevel.uppercore.util.nms.impl.entity;
 
+import org.bukkit.Bukkit;
 import org.bukkit.FireworkEffect;
 import org.bukkit.Location;
 import org.bukkit.entity.Firework;
 import org.bukkit.inventory.meta.FireworkMeta;
+import xyz.upperlevel.uppercore.Uppercore;
 import xyz.upperlevel.uppercore.util.nms.NmsPacket;
+import xyz.upperlevel.uppercore.util.nms.impl.WorldNms;
 import xyz.upperlevel.uppercore.util.nms.refl.Class;
 import xyz.upperlevel.uppercore.util.nms.refl.field.IntField;
 
 public final class FireworkNms {
     private static final Class CLASS;
-    private static final IntField expectedLifespan;
-    private static final IntField ticksFlown;
 
     static  {
         CLASS = Class.of(NmsPacket.NMS, "EntityFireworks");
-        expectedLifespan = CLASS.getIntField("expectedLifespan", true);
-        ticksFlown = CLASS.getIntField("ticksFlown", false);
-    }
-
-    public static void instantFirework(Firework firework) {
-        /*
-        EntityFirework handle = ((CraftFirework)firework).getHandle();
-        handle.ticksFlown = handle.expectedLifespan - 1;
-         */
-        Object handle = EntityNms.getHandle(firework);
-        ticksFlown.set(handle, expectedLifespan.get(handle));
     }
 
     public static void instantFirework(Location loc, FireworkEffect effect) {
@@ -33,6 +23,10 @@ public final class FireworkNms {
         FireworkMeta meta = firework.getFireworkMeta();
         meta.addEffect(effect);
         firework.setFireworkMeta(meta);
-        instantFirework(firework);
+        Bukkit.getScheduler().runTaskLater(
+                Uppercore.get(),
+                firework::detonate,
+                1
+        );
     }
 }
