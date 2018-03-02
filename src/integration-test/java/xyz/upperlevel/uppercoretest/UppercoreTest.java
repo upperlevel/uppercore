e2bdbe5b757d03d14447383b92e91d376ba9e8e0
loryruta <loryruta23@gmail.com>
2018-03-02 16:26:18 +0100

    Add integration-test

---
+++ src/integration-test/java/xyz/upperlevel/uppercoretest/UppercoreTest.java
@@ -0,0 +1,13 @@
+package xyz.upperlevel.uppercoretest;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class UppercoreTest extends JavaPlugin {
+    @Override
+    public void onEnable() {
+    }
+
+    @Override
+    public void onDisable() {
+    }
+}
