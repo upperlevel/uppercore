0c15f8fb1a147a14753379e936399fab178c2c34
loryruta <loryruta23@gmail.com>
2019-08-08 22:10:07 +0200

    New scoreboard entry generation algorithm

--- src/main/java/xyz/upperlevel/uppercore/board/Board.java
+++ src/main/java/xyz/upperlevel/uppercore/board/Board.java
@@ -11,6 +11,7 @@ import org.bukkit.scoreboard.Team;
 import xyz.upperlevel.uppercore.placeholder.PlaceholderRegistry;
 import xyz.upperlevel.uppercore.placeholder.PlaceholderValue;
 
+import java.io.StringReader;
 import java.util.*;
 import java.util.stream.Collectors;
 
@@ -73,12 +74,16 @@ public class Board {
         this.lines = lines;
     }
 
-    public void clearLines() {
-        lines.clear();
+    private void clearEntries() {
         for (String entry : entries) {
             scoreboard.resetScores(entry);
         }
         entries.clear();
+    }
+
+    public void clearLines() {
+        lines.clear();
+        clearEntries();
     }
 
     public void set(BoardModel model, Player reference) {
@@ -103,39 +108,97 @@ public class Board {
         }
     }
 
+    public static int generateEntry(Set<String> entries, StringBuilder entry, String padding) {
+        int size = entry.length();
+
+        // Forward
+        while (entries.contains(entry.toString())) {
+            entry.append(padding);
+            if (entry.length() >= MAX_TEAM_ENTRY_CHARACTERS) {
+                if (entry.length() > MAX_TEAM_ENTRY_CHARACTERS) {
+                    entry.delete(MAX_TEAM_ENTRY_CHARACTERS + 1, entry.length());
+                }
+                break;
+            }
+        }
+
+        // Backward
+        while (entries.contains(entry.toString())) {
+            int from = size - padding.length();
+            if (from < 0) {
+                throw new IllegalStateException("Invalid board line, can't avoid repetition.");
+            }
+            entry.replace(from, size, padding);
+            size -= padding.length();
+        }
+
+        return size;
+    }
+
     private void updateLine(int position, Player player, PlaceholderRegistry placeholders) {
         String line = lines.get(position).resolve(player, placeholders);
         Team team = getTeam(position);
 
         int startAt, endAt;
+        String lastColor;
+
+        // ================================================================================
+        // Prefix
+        // ================================================================================
 
         startAt = 0;
         endAt = Math.min(line.length(), startAt + MAX_TEAM_PREFIX_CHARACTERS);
-        team.setPrefix(line.substring(startAt, endAt));
+
+        String prefix = line.substring(startAt, endAt);
+        team.setPrefix(prefix);
+        lastColor = ChatColor.getLastColors(prefix);
+
+        // ================================================================================
+        // Entry
+        // ================================================================================
 
         startAt = endAt;
-        endAt = Math.min(line.length(), startAt + MAX_TEAM_ENTRY_CHARACTERS);
-        StringBuilder padding = new StringBuilder();
-        String entry;
-        do {
-            entry = line.substring(startAt, Math.max(endAt - padding.length(), 0)) + padding;
-            String color = ChatColor.RESET.toString();
-            padding.append(color);
-        } while (entries.contains(entry));
-        if (entry.length() > MAX_TEAM_ENTRY_CHARACTERS) {
-            throw new IllegalStateException("Invalid team entry size: " + entry.length() + " > " + MAX_TEAM_ENTRY_CHARACTERS);
-        }
+
+        // The entry must take care of the prefix color.
+        endAt = Math.min(line.length(), startAt + MAX_TEAM_ENTRY_CHARACTERS - lastColor.length());
+
+        // Generates the entry, with the lastColor + entry.
+        StringBuilder digest = new StringBuilder(lastColor + line.substring(startAt, endAt));
+        int size = generateEntry(entries, digest, ChatColor.RESET.toString());
+        String entry = digest.toString();
+
+        // The starting index remains the same, the ending index is minus the lastColor length.
+        // startAt = startAt;
+        endAt = startAt + Math.max(size - lastColor.length(), 0);
+
+        // The entry lastColor is influenced by previous lastColor.
+        lastColor = ChatColor.getLastColors(entry.substring(0, size));
+
         entries.add(entry);
         team.addEntry(entry);
         objective.getScore(entry).setScore(MAX_SCOREBOARD_LINES - position);
 
-        startAt = Math.max(endAt - padding.length(), 0);
-        endAt = Math.min(line.length(), startAt + MAX_TEAM_SUFFIX_CHARACTERS);
-        team.setSuffix(line.substring(startAt, endAt));
+        // ================================================================================
+        // Suffix
+        // ================================================================================
+
+        startAt = endAt;
+        endAt = Math.min(line.length(), startAt + MAX_TEAM_SUFFIX_CHARACTERS - lastColor.length());
+        String suffix = lastColor + line.substring(startAt, endAt);
+
+        // The suffix was cut since the line was too long!
+        if (endAt < line.length()) {
+            String cross = ChatColor.RED + "X";
+            suffix = new StringBuilder(suffix)
+                    .replace(suffix.length() - cross.length(), suffix.length(), cross)
+                    .toString();
+        }
+
+        team.setSuffix(suffix);
     }
 
     private void updateLines(Player player, PlaceholderRegistry placeholders) {
-        entries.clear();
+        clearEntries();
         for (int position = 0; position < lines.size(); position++) {
             updateLine(position, player, placeholders);
         }
