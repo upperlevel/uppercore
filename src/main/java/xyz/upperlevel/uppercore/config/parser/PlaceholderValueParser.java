d574d575d8dd73f75da90c5481a1a59230a1356a
Rossi Lorenzo <snowycoder@gmail.com>
2019-09-26 19:19:30 +0200

    Add color translation to PlaceholderValue parsing

--- src/main/java/xyz/upperlevel/uppercore/config/parser/PlaceholderValueParser.java
+++ src/main/java/xyz/upperlevel/uppercore/config/parser/PlaceholderValueParser.java
@@ -1,31 +1,40 @@
 package xyz.upperlevel.uppercore.config.parser;
 
-import org.bukkit.plugin.Plugin;
 import org.yaml.snakeyaml.nodes.Node;
 import org.yaml.snakeyaml.nodes.NodeId;
 import org.yaml.snakeyaml.nodes.ScalarNode;
 import xyz.upperlevel.uppercore.placeholder.PlaceholderUtil;
 import xyz.upperlevel.uppercore.placeholder.PlaceholderValue;
+import xyz.upperlevel.uppercore.util.TextUtil;
 
 public class PlaceholderValueParser extends ConfigParser {
     private final ConfigParser valueParser;
+    private final boolean translateColors;
 
     public PlaceholderValueParser(ConfigParser valueParser) {
+        this(valueParser, valueParser.getHandleClass().equals(String.class));
+    }
+
+    public PlaceholderValueParser(ConfigParser valueParser, boolean translateColors) {
         super(PlaceholderValue.class);
         this.valueParser = valueParser;
+        this.translateColors = translateColors;
     }
 
     @Override
     public PlaceholderValue parse(Node root) {
         checkNodeId(root, NodeId.scalar);
         ScalarNode node = (ScalarNode) root;// TODO: support more complex types like PlaceholderValue<List<String>> or similar
-        //TODO Test Translate colors and such
+        if (translateColors) {
+            node = replaceValue(node, TextUtil.translatePlain(node.getValue()));
+        }
         if (!PlaceholderUtil.hasPlaceholders(node.getValue())) {
             return PlaceholderValue.fake(valueParser.parse(node));
         } else {
             // Replace the node's value with the resolved placeholders value
+            ScalarNode n = node;
             return (player, local) ->
-                    valueParser.parse(replaceValue(node, PlaceholderUtil.resolve(player, node.getValue(), local)));
+                    valueParser.parse(replaceValue(n, PlaceholderUtil.resolve(player, n.getValue(), local)));
         }
     }
 }
