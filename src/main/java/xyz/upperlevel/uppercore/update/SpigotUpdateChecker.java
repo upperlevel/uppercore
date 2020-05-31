445c20dd827e9f222664e5d7780f50d999dac6a3
Rossi Lorenzo <snowycoder@gmail.com>
2020-05-31 21:56:05 +0200

    Fix update checker

--- src/main/java/xyz/upperlevel/uppercore/update/SpigotUpdateChecker.java
+++ src/main/java/xyz/upperlevel/uppercore/update/SpigotUpdateChecker.java
@@ -10,28 +10,25 @@ import java.net.URL;
 import java.nio.charset.StandardCharsets;
 
 public class SpigotUpdateChecker extends UpdateChecker {
-    public static final String API_URL = "http://www.spigotmc.org/api/general.php";
-    public static final String API_KEY = "98BE0FE67F88AB82B4C197FAF1DC3B69206EFDCC4D3B80FC83A00037510B99B4";
+    public static final String API_URL = "https://api.spigotmc.org/legacy/update.php?resource=";
 
-    private final int resId;
+    private final long resId;
 
     public SpigotUpdateChecker(Plugin plugin, String spigotFullId) {
         super(plugin, spigotFullId);
         String[] parts = spigotFullId.split("\\.");
         try {
-            resId = Integer.parseInt(parts[parts.length - 1]);
+            resId = Long.parseLong(parts[parts.length - 1]);
         } catch (Exception e) {
             throw new IllegalArgumentException("Invalid id: " + spigotFullId);
         }
     }
-
-
+    
     @Override
     public String fetchVersion() throws IOException {
-        HttpURLConnection connection = (HttpURLConnection) new URL(API_URL).openConnection();
+        HttpURLConnection connection = (HttpURLConnection) new URL(API_URL + resId).openConnection();
         connection.setDoOutput(true);
-        connection.setRequestMethod("POST");
-        connection.getOutputStream().write(("key=" + API_KEY + "&resource=" + resId).getBytes(StandardCharsets.UTF_8));
+        connection.setRequestMethod("GET");
         return (new BufferedReader(new InputStreamReader(connection.getInputStream()))).readLine();
     }
 }
