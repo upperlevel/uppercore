f85c78196daeb36f6d00d170b07fb7d6d470c70e
loryruta <loryruta23@gmail.com>
2020-08-31 21:36:45 +0200

    Fix DynLib API

--- src/main/java/xyz/upperlevel/uppercore/util/DynLib.java
+++ src/main/java/xyz/upperlevel/uppercore/util/DynLib.java
@@ -1,5 +1,6 @@
 package xyz.upperlevel.uppercore.util;
 
+import org.bukkit.Bukkit;
 import xyz.upperlevel.uppercore.Uppercore;
 
 import java.io.File;
@@ -9,55 +10,82 @@ import java.net.MalformedURLException;
 import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.StandardCopyOption;
-import java.util.function.Predicate;
+import java.util.List;
+import java.util.logging.Logger;
+import java.util.stream.Collectors;
 
-public final class DynLib {
-    private DynLib() {
+public class DynLib {
+    private final URL url;
+    private final File file;
+
+    private final String id; // Just for debug purposes.
+
+    public DynLib(URL url) {
+        this.url = url;
+        this.file = getFile();
+        this.id = file.getName();
+        Dbg.pf("The file for %s is: %s", id, file);
     }
 
-    public static void download(URL url, File file) throws IOException {
-        Uppercore.logger().fine(String.format("Downloading: %s", url));
-        try (InputStream in = url.openStream()) {
-            Files.copy(in, file.toPath(), StandardCopyOption.REPLACE_EXISTING);
+    private File getFile() {
+        File file = new File(Bukkit.getWorldContainer(), "dyn_libs" + File.separator + new File(url.getPath()).getName());
+        file.getParentFile().mkdir(); // In order to create dyn_libs folder.
+        return file;
+    }
+
+    public void download() throws IOException {
+        Logger logger = Uppercore.logger();
+        if (file.exists()) {
+            logger.info(String.format("Lib already downloaded: %s", id));
+            return;
+        }
+        logger.info(String.format("Downloading: %s", id));
+        if (file.createNewFile()) {
+            try (InputStream in = url.openStream()) {
+                Files.copy(in, file.toPath(), StandardCopyOption.REPLACE_EXISTING);
+            }
         }
     }
 
-    public static void load(File file) throws MalformedURLException {
-        Uppercore.logger().fine(String.format("Loading: %s", file.getName()));
+    public void load() throws MalformedURLException {
+        Logger logger = Uppercore.logger();
+        if (!file.exists()) {
+            logger.info(String.format("Lib not found at: %s", file));
+            return;
+        }
+        logger.info(String.format("Loading: %s", id));
         ExternalJarUtil.addUrl(file);
     }
 
-    public static boolean isLoaded(String classpath) {
-        try {
-            Class.forName(classpath);
-            return true;
-        } catch (ClassNotFoundException e) {
-            return false;
+    public static class Pool {
+        private final List<DynLib> libs;
+
+        public Pool(List<DynLib> libs) {
+            this.libs = libs;
+        }
+
+        public void install() throws IOException {
+            // First downloads all libraries and then loads them.
+            for (DynLib lib : libs) lib.download();
+            for (DynLib lib : libs) lib.load();
         }
     }
 
-    public static Predicate<File> req(String url) {
-        return folder -> {
+    public static Pool parsePool(List<String> libs) {
+        return new Pool(libs.stream().map(url -> {
             try {
-                URL parsedUrl = new URL(url);
-                File file = new File(folder, new File(parsedUrl.getPath()).getName());
-                download(parsedUrl, file);
-                load(file);
-                return true;
-            } catch (IOException e) {
-                throw new IllegalStateException(e);
+                return new DynLib(new URL(url));
+            } catch (MalformedURLException e) {
+                throw new IllegalArgumentException(e);
             }
-        };
+        }).collect(Collectors.toList()));
     }
 
-    public static Predicate<File> check(String classpath) {
-        return folder -> isLoaded(classpath);
-    }
-
-    public static Predicate<File> reqAndCheck(String url, String classpath) {
-        return folder -> {
-            req(url).test(folder);
-            return check(classpath).test(folder);
-        };
+    public static void checkAssert(String classpath) {
+        try {
+            Class.forName(classpath);
+        } catch (ClassNotFoundException e) {
+            throw new IllegalStateException(e);
+        }
     }
 }
