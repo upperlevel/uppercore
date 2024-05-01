44d13f54cb4c411cf602430d681471ab27c3a110
Lorenzo Rossi <snowycoder@gmail.com>
2024-05-01 15:08:02 +0200

    Add nix flake and update gradle

---
+++ flake.nix
@@ -0,0 +1,22 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+  outputs = { self, nixpkgs, flake-utils}:
+    flake-utils.lib.eachDefaultSystem
+      (system:
+        let
+          overlays = [ ];
+          pkgs = import nixpkgs {
+            inherit system overlays;
+          };
+        in
+        with pkgs;
+        {
+          devShells.default = mkShell {
+            buildInputs = [ jdk gradle_7 ];
+          };
+        }
+      );
+}
